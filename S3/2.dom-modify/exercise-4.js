1d380f5602c4a908156881ab13ed3d10590bc09c
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-20 21:35:07 +0100

    feat exercisse4

---
+++ S3/2.dom-modify/exercise-4.js
@@ -0,0 +1,4 @@
+"use strict";
+//Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
+const texto3 = document.createTextNode("Soy dinámico");
+document.body.append(texto3);
