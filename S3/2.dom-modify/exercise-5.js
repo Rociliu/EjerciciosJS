622caf17a338d58de17f036f1608fa25bb4860eb
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-20 21:36:52 +0100

    feat exercise5

---
+++ S3/2.dom-modify/exercise-5.js
@@ -0,0 +1,4 @@
+"use strict";
+// Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
+const h2 = document.querySelector('.fn-insert-here');
+h2.innerText = "Wubba Lubba dub dub";
