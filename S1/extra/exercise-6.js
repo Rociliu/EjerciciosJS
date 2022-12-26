45b123022b7528e4854c9a503f65f10866a4b30f
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-26 19:26:56 +0100

    feat exercise6

---
+++ S1/extra/exercise-6.js
@@ -0,0 +1,21 @@
+"use strict";
+//Usa un bucle para recorrer el array de productos (`products`) y añade al array `goodProducts` los que tengan más de 20 ventas (`sellCount`) y al array `badProducts` los que tengan menos.
+
+const goodProducts = [];
+const badProducts = [];
+const products = [
+{name: 'Gorra de rodilla', sellCount: 10},
+{name: 'Pantalón de pana', sellCount: 302},
+{name: 'Reloj de papel albal', sellCount: 23},
+{name: 'Impar de zapatos', sellCount: 6}];
+
+for (let product of products){
+    if (product.sellCount < 20){
+        badProducts.push(product.name)
+    } else {
+        goodProducts.push(product.name)
+    }
+}
+console.log("Los más vendidos son: " + goodProducts);
+console.log("Los menos vendidos son: " + badProducts);
+
