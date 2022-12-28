a861720de0f8621ff02f06e101b67d062a295f63
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-29 00:09:56 +0100

    feat exercise3

--- S1/extra/exercise-3.js
+++ S1/extra/exercise-3.js
@@ -6,23 +6,28 @@ const movies = [
 {name: "Origen", durationInMinutes: 165}, 
 {name: "El señor de los anillos", durationInMinutes: 967}, 
 {name: "Solo en casa", durationInMinutes: 214}, 
-{name: "El jardin de las palabras", durationInMinutes: 40}];
+{name: "El jardin de las palabras", durationInMinutes: 40}
+];
 
 const shortMovies = [];
 const mediumMovies = [];
 const largeMovies = [];
 
 for (let film of movies){
-    if (film.durationInMinutes<100){
-        shortMovies.push(film.name, film.durationInMinutes);
-    } else if (film.durationInMinutes>200){
-        largeMovies.push(film.name, film.durationInMinutes);
+    if (film.durationInMinutes < 100){
+        shortMovies.push(film);
+    } else if (film.durationInMinutes > 200){
+        largeMovies.push(film);
     }else{
-        mediumMovies.push(film.name, film.durationInMinutes)
+        mediumMovies.push(film)
     }
 }
-console.log("Películas de corta duración: " + shortMovies);
-console.log("Películas de media duración: " + mediumMovies);
-console.log("Películas de larga duración: " + largeMovies);
+
+console.log("Películas de corta duración: ")
+console.log(shortMovies);
+console.log("Películas de media duración: ");
+console.log(mediumMovies);
+console.log("Películas de larga duración: ");
+console.log(largeMovies);
 
 
