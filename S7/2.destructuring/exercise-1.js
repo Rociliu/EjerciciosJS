1579b079e96babc6036c5b93454d452fd4616d08
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-31 10:40:17 +0100

    feat exercise1

---
+++ S7/2.destructuring/exercise-1.js
@@ -0,0 +1,11 @@
+"use strict";
+const game = {
+    title: 'The last us 2', 
+    gender: ['action', 'zombie', 'survival'],
+    year: 2020
+}
+
+const {title, gender, year} = game;
+console.log(title);
+console.log(gender);
+console.log(year);
