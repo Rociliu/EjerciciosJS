49841e1c2803d7d25734a2844cf4dc51761f3f26
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-31 13:51:48 +0100

    feat exercise4

--- S7/2.destructuring/exercise-4.js
+++ S7/2.destructuring/exercise-4.js
@@ -4,6 +4,7 @@ const car = {
     itv: [2015, 2011, 2020]
 }
 let {name, itv} = car;
-console.log(name, itv);
+console.log(`El modelo del vehículo es ${name}`);
+console.log(`Las revisiones se han pasado en ${itv}`);
 let[year1, year2, year3] = itv;
 console.log(year1, year2, year3);
