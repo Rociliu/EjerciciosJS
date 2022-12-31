daf17e2757371eaa9db78bce226eaf2d507fccd5
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-31 13:06:52 +0100

    feat exercise4

---
+++ S7/2.destructuring/exercise-4.js
@@ -0,0 +1,9 @@
+"use strict";
+const car = {
+    name: 'Mazda 6', 
+    itv: [2015, 2011, 2020]
+}
+let {name, itv} = car;
+console.log(name, itv);
+let[year1, year2, year3] = itv;
+console.log(year1, year2, year3);
