f9ab4bc85f671c8d09934d5ea57e6790728c4ffb
Rociliu <mrrosa.carrasco@gmail.com>
2023-01-21 21:35:07 +0100

    feat exercise2-a

--- S8/2.fetch/exercise-2-a.js
+++ S8/2.fetch/exercise-2-a.js
@@ -1,8 +1,7 @@
 const button$$ = document.querySelector('button');
-//button$$.addEventListener('click',consultaApi);
 
-const consultaApi = async (valorDelInput) => {
-    const answer = await fetch('https://api.nationalize.io?name=' + valorDelInput);
+const consultaApi = async (inputValue) => {
+    const answer = await fetch('https://api.nationalize.io?name=' + inputValue);
     const data = await answer.json();
     console.log(data);
 } 
